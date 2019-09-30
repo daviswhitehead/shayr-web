ca7a47aa54c3d0c3575ad13138b64ca358f785ba
Davis Whitehead <whitehead.davis@gmail.com>
2019-09-30 16:21:05 -0400

    Updates

---
+++ precache-manifest.5edf02e707ecc12e794e41877be28bfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fafa06f0bb0df49487cac08d030ef2",
+    "url": "/shayr-web/index.html"
+  },
+  {
+    "revision": "36e4f4d90034cde24442",
+    "url": "/shayr-web/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f77a225a457443df062e",
+    "url": "/shayr-web/static/js/2.a6759004.chunk.js"
+  },
+  {
+    "revision": "36e4f4d90034cde24442",
+    "url": "/shayr-web/static/js/main.a84ef87b.chunk.js"
+  },
+  {
+    "revision": "18bb40dacc8d0d710c31",
+    "url": "/shayr-web/static/js/runtime-main.cbec9548.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shayr-web/static/media/logo.25bf045c.svg"
+  }
+]);
