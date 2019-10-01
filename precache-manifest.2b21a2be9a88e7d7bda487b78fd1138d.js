3ef83aa634c4c93cf385b5b870cc25c02b58cde1
Davis Whitehead <whitehead.davis@gmail.com>
2019-10-01 16:29:42 -0400

    Updates

---
+++ precache-manifest.2b21a2be9a88e7d7bda487b78fd1138d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59aaa3960b46917ced73723e8d41348f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfaa182cd4a3cc92c97c",
+    "url": "/static/css/main.237a9955.chunk.css"
+  },
+  {
+    "revision": "62d1861c94cde8c6a9e2",
+    "url": "/static/js/2.b230d921.chunk.js"
+  },
+  {
+    "revision": "cfaa182cd4a3cc92c97c",
+    "url": "/static/js/main.d40d0e50.chunk.js"
+  },
+  {
+    "revision": "eea3a1fa1857d2426e20",
+    "url": "/static/js/runtime-main.84d0cf64.js"
+  },
+  {
+    "revision": "08ef5d7a2e3e27223df1b1db25b65b82",
+    "url": "/static/media/NunitoSans-Black.08ef5d7a.ttf"
+  },
+  {
+    "revision": "26fe63cd6deb50cea5e2119f79f9eb76",
+    "url": "/static/media/NunitoSans-BlackItalic.26fe63cd.ttf"
+  },
+  {
+    "revision": "51066f4d1d33630cd761e8cd0168d7b0",
+    "url": "/static/media/NunitoSans-Bold.51066f4d.ttf"
+  },
+  {
+    "revision": "cdd41ae4a67b8e24ba8951ac348eeb63",
+    "url": "/static/media/NunitoSans-BoldItalic.cdd41ae4.ttf"
+  },
+  {
+    "revision": "e6a9e16cac802736f3c92c47d94a52de",
+    "url": "/static/media/NunitoSans-ExtraBold.e6a9e16c.ttf"
+  },
+  {
+    "revision": "b6997ae0a357ed60d3aff774cb5bd5a0",
+    "url": "/static/media/NunitoSans-ExtraBoldItalic.b6997ae0.ttf"
+  },
+  {
+    "revision": "bc36a8726e20804a94da3a12bd6eda84",
+    "url": "/static/media/NunitoSans-ExtraLight.bc36a872.ttf"
+  },
+  {
+    "revision": "45e071e1be37d12ab5fabb4c2e70a149",
+    "url": "/static/media/NunitoSans-ExtraLightItalic.45e071e1.ttf"
+  },
+  {
+    "revision": "740ec18903919403ad977e6e10e65a29",
+    "url": "/static/media/NunitoSans-Italic.740ec189.ttf"
+  },
+  {
+    "revision": "3a318cfcc78904616af13303520a69a1",
+    "url": "/static/media/NunitoSans-Light.3a318cfc.ttf"
+  },
+  {
+    "revision": "0510d4334ff70f118abccaeeab9caf74",
+    "url": "/static/media/NunitoSans-LightItalic.0510d433.ttf"
+  },
+  {
+    "revision": "fb98ed1700e8dfaf0764c11fc36a0a05",
+    "url": "/static/media/NunitoSans-Regular.fb98ed17.ttf"
+  },
+  {
+    "revision": "9c443ad7c0f391c4854a6b7c0f52788b",
+    "url": "/static/media/NunitoSans-SemiBold.9c443ad7.ttf"
+  },
+  {
+    "revision": "0e3a7dbb09be7a4154f87244f3e1c4e4",
+    "url": "/static/media/NunitoSans-SemiBoldItalic.0e3a7dbb.ttf"
+  },
+  {
+    "revision": "5c9d9da2dfaab3d4eaf7accc7159af77",
+    "url": "/static/media/compiled.5c9d9da2.mov"
+  },
+  {
+    "revision": "be76d5ff72c2ee19ab29e719ccb21867",
+    "url": "/static/media/shayr-icon.be76d5ff.png"
+  },
+  {
+    "revision": "6e5e9a16fe2aa6fe78d7f345c01c0e1c",
+    "url": "/static/media/shayr-logo.6e5e9a16.svg"
+  },
+  {
+    "revision": "3f8eb852e3bf076d379d951e104e46c5",
+    "url": "/static/media/vector-share-google.3f8eb852.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shayr-web/precache-manifest.43fef3944329a7c2bb68cfbee57fe0b4.js"
+  "/precache-manifest.2b21a2be9a88e7d7bda487b78fd1138d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shayr-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
