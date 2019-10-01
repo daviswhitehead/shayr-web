45a9ee70bb784079602d3581e7d16af79320f775
Davis Whitehead <whitehead.davis@gmail.com>
2019-10-01 17:38:07 -0400

    switching to hash navigation

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter, Switch, Route} from "react-router-dom";
 import {screenPaths, HOME, HOW_TO, PRIVACY_POLICY, TERMS_AND_CONDITIONS} from './Routes'
 import Home from '../screens/Home'
 import HowTo from '../screens/HowTo'
@@ -9,21 +9,13 @@ import TermsAndConditions from '../screens/TermsAndConditions'
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter> {/* basename={process.env.PUBLIC_URL} */}
       <Switch> {/* returns the FIRST matching path */}
-        <Route path={screenPaths[HOW_TO]}>
-          <HowTo />
-        </Route>
-        <Route path={screenPaths[PRIVACY_POLICY]}>
-          <PrivacyPolicy />
-        </Route>
-        <Route path={screenPaths[TERMS_AND_CONDITIONS]}>
-          <TermsAndConditions />
-        </Route>
-        <Route path={screenPaths[HOME]}>
-          <Home />
-        </Route>
+        <Route path={screenPaths[HOW_TO]} component={HowTo} />
+        <Route path={screenPaths[PRIVACY_POLICY]} component={PrivacyPolicy} />
+        <Route path={screenPaths[TERMS_AND_CONDITIONS]} component={TermsAndConditions} />
+        <Route exact path={screenPaths[HOME]} component={Home} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
