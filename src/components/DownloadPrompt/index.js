b7740f672d56bd32d8de3bd67665362f8bea7aac
Davis Whitehead <whitehead.davis@gmail.com>
2019-10-07 12:48:38 -0400

    updated download links

--- src/components/DownloadPrompt/index.js
+++ src/components/DownloadPrompt/index.js
@@ -13,7 +13,7 @@ export default class extends Component {
         alignItems: 'left'
       }}
       >
-        <a href='https://www.google.com'>
+        <a href='https://apps.apple.com/app/id1450594568'>
         <img src='https://linkmaker.itunes.apple.com/images/badges/en-us/badge_appstore-lrg.svg' alt='iOS Store Button' style={{
             width: 126,
             marginTop: 10,
@@ -21,7 +21,7 @@ export default class extends Component {
             marginRight: 10
           }}/>
         </a>
-        <a href='https://www.google.com' >
+        <a href='https://play.google.com/store/apps/details?id=com.daviswhitehead.shayr.android.prod' >
         <img src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png' alt='Android Store Button' style={{
             width: 144,
           }}/>
